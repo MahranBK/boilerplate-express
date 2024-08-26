e898122d302e40b2e791ff4fbfd06945f2af6714
Mahran <benkhamsamahran@isamm.u-manouba.tn>
2024-08-26 17:15:41 +0100

    added get method to display text msg when user visits root domain

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
-let express = require('express');
-let app = express(); 
+let express = require('express');//require('express') function loads the Express module & express variable now holds a reference to this module
+let app = express();//creates an instance of an Express application that u'll use to define routes and configure your web application.
 
-
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+});
 
 
 
