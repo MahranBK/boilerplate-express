abe7bbd765f934d829b327a5dceeddb94d033eaf
Mahran Ben Khamsa <mahran.bk15@gmail.com>
2024-09-03 00:03:23 +0100

    served html file on redirect to root added middleware to serve static assets served json on redirect to /json

--- myApp.js
+++ myApp.js
@@ -1,13 +1,18 @@
 let express = require('express');//require('express') function loads the Express module & express variable now holds a reference to this module
-let app = express();//creates an instance of an Express application that u'll use to define routes and configure your web application.
+let app = express();//creates an instance of an Express app that u'll use to define routes and configure your web app.
 
+absolutePath = __dirname + '/views/index.html';
 app.get('/', (req, res) => {
-    res.send('Hello Express');
+    
+    res.sendFile(absolutePath);
 });
 
+absPath = __dirname + '/public';
+app.use('/public', express.static(absPath));
 
-
-
+app.get('/json', (req, res) => {
+    res.json({"message": "Hello json"});
+});
 
 
 
