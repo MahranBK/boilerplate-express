f6ad1664a4c1d5b35a3e533f49a2d5c8c5193048
Mahran Ben Khamsa <mahran.bk15@gmail.com>
2024-09-07 00:24:52 +0100

    added a middleware that logs info about every request received

--- myApp.js
+++ myApp.js
@@ -2,9 +2,13 @@ require('dotenv').config();//dotenv package loads environment variables from you
 let express = require('express');//require('express') function loads the Express module & express variable now holds a reference to this module
 let app = express();//creates an instance of an Express app that u'll use to define routes and configure your web app.
 
+app.use((req, res, next) => {
+    console.log(req.method+' '+req.path+' - '+req.ip);
+    next();
+});
+
 absolutePath = __dirname + '/views/index.html';
 app.get('/', (req, res) => {
-    
     res.sendFile(absolutePath);
 });
 
@@ -19,7 +23,6 @@ app.get('/json', (req, res) => {
         res.json({"message": "Hello json"});
     }
 });
-
 
 
 
